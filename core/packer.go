73f3913a1133c3578f675fe31490ba7b9451a7bd
gfxv <superfreezu@gmail.com>
2024-07-16 18:46:30 +0300

    feat: basic zlib compression and decompression

---
+++ core/packer.go
@@ -0,0 +1,37 @@
+package core
+
+import (
+	"bytes"
+	"compress/zlib"
+	"io"
+)
+
+
+type PackFunc func([]byte) []byte
+type UnpackFunc func([]byte) ([]byte, error)
+
+
+// TODO: add levels of compression
+
+func ZLibPack(data []byte) []byte {
+	var buff bytes.Buffer
+    w := zlib.NewWriter(&buff)
+    w.Write(data)
+    w.Close()
+    return buff.Bytes() 
+}
+
+func ZLibUnpack(data []byte) ([]byte, error) {
+    b := bytes.NewReader(data)
+    r, err := zlib.NewReader(b)
+    if err != nil {
+        return nil, err
+    }
+
+    var result bytes.Buffer
+    if _, err = io.Copy(&result, r); err != nil {
+        return nil, err
+    }
+
+    return result.Bytes(), nil
+}
