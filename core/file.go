ed02687ac51b07bcd7b13f1841e7e756307913c4
gfxv <superfreezu@gmail.com>
2024-07-19 18:00:39 +0300

    feat: file tree parsing

---
+++ core/file.go
@@ -0,0 +1,55 @@
+package core
+
+import (
+    "os"
+    "path/filepath"
+)
+
+
+// File represents a node in a directory tree.
+type File struct {
+    FullPath string    
+    FileName string
+    IsDir    bool
+    Children []File   
+}
+
+// Create directory hierarchy.
+func NewFileTree(root string) (result *File, err error) {
+    nodes := make(map[string]*File)
+    absRoot, err := filepath.Abs(root)
+    if err != nil {
+	    return
+    }
+
+    walkFunc := func(path string, info os.FileInfo, err error) error {
+    	if err != nil {
+    		return err
+    	}   
+
+	    nodes[path] = &File {
+	    	FullPath: path,
+            FileName: info.Name(),
+            IsDir:    info.IsDir(),
+	    	Children: make([]File, 0),
+	    }
+
+	    return nil
+    }
+
+    if err = filepath.Walk(absRoot, walkFunc); err != nil {
+    	return
+    }
+
+    for path, node := range nodes {
+    	parentPath := filepath.Dir(path)
+    	parent, exists := nodes[parentPath]
+    	if !exists { // If a parent does not exist, this is the root.
+    		result = node
+    	} else {
+    		parent.Children = append(parent.Children, *node)
+    	}
+    }
+    return
+}
+
