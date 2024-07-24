ede8bcff870b67f06893a1925cc7856360184bff
gfxv <superfreezu@gmail.com>
2024-07-24 22:24:50 +0300

    feat!: add better way of storing files and (sub)dirs

--- core/storage.go
+++ core/storage.go
@@ -13,17 +13,22 @@ import (
 type TransformPathFunc func([]byte) (string, string)
 
 func DefaultTransformPathFunc(data []byte) (prefix string, filename string) {
-    fullHash := sha1.Sum(data)
-    strHash := hex.EncodeToString(fullHash[:])
-    prefix, filename = strHash[:5], strHash[5:]
-    return
+	fullHash := sha1.Sum(data)
+	strHash := hex.EncodeToString(fullHash[:])
+	prefix, filename = strHash[:5], strHash[5:]
+	return
+}
+
+func transformKey(key string) string {
+	keyHash := sha1.Sum([]byte(key))
+	return hex.EncodeToString(keyHash[:])
 }
 
 type Storage struct {
-    baseDir       string
-    transformPath TransformPathFunc
-    pack          PackFunc
-    unpack        UnpackFunc
+	baseDir       string
+	transformPath TransformPathFunc
+	pack          PackFunc
+	unpack        UnpackFunc
 }
 
 func NewDefaultStorage() *Storage {
@@ -41,83 +46,75 @@ func (s *Storage) WithTransformPathFunc(pathFunc TransformPathFunc) *Storage {
 }
 
 func (s *Storage) WithCompressionFuncs(compress PackFunc, decompress UnpackFunc) *Storage {
-    s.pack = compress
-    s.unpack = decompress
-    return s
+	s.pack = compress
+	s.unpack = decompress
+	return s
 }
 
 func (s *Storage) Has(path string) bool {
-    _, err := os.Stat(path)
-    return !errors.Is(err, os.ErrNotExist)
+	_, err := os.Stat(path)
+	return !errors.Is(err, os.ErrNotExist)
 }
-
 
 // Store receives path (or multiple paths) to file or directory that should be saved on the disk
 func (s *Storage) Store(key string, paths ...string) error {
-    var err error
-    for _, p := range paths {
-        tree, err := NewFileTree(p)
-        if err != nil {
-            return err
-        }
-        err = s.saveTree(key, *tree)
-    }
-    return err
+	var err error
+	// transformedKey := transformKey(key)
+	for _, p := range paths {
+		tree, err := NewTree(p)
+		if err != nil {
+			return err
+		}
+		err = s.saveTree(key, tree)
+	}
+	return err
 }
 
-func (s *Storage) saveTree(key string, tree ...File) error {
-    var err error
-    for _, t := range tree {
-        if t.IsDir {
-            s.saveTree(key, t.Children...)
-        }
-        err = s.writeFile(key, t.FullPath)
-    }
-    return err
-}
-
-func (s *Storage) writeFile(key, path string) error {
-    file, err := os.ReadFile(path)
-    if err != nil {
-        return err
-    }
-    return s.Write(key, file)
+func (s *Storage) saveTree(key string, tree []string) error {
+	var err error
+	for _, t := range tree {
+		file, err := os.ReadFile(t)
+		if err != nil {
+			return err
+		}
+		err = s.Write(key, file)
+	}
+	return err
 }
 
 func (s *Storage) Read(key string) {
 }
 
-// TODO: save key to db 
-// Write 
+// TODO: save key to db
+// Write ..
 func (s *Storage) Write(key string, data []byte) error {
-    prefix, filename := s.transformPath(data)
+	prefix, filename := s.transformPath(data)
 
-    folders := fmt.Sprintf("%s/%s", s.baseDir, prefix)
-    if err := os.MkdirAll(folders, os.ModePerm); err != nil { // TODO: change permissions (?), now - 777
-        return err
-    }
+	folders := fmt.Sprintf("%s/%s", s.baseDir, prefix)
+	if err := os.MkdirAll(folders, os.ModePerm); err != nil { // TODO: change permissions (?), now - 777
+		return err
+	}
 
-    fullPath := fmt.Sprintf("%s/%s", folders, filename)
+	fullPath := fmt.Sprintf("%s/%s", folders, filename)
 
-    if s.Has(fullPath) {
-        return fmt.Errorf("collision detected! \nkey '%s' with data provided already exists", key)
-    }
+	if s.Has(fullPath) {
+		return fmt.Errorf("collision detected! \nkey '%s' with data provided already exists", key)
+	}
 
-    file, err := os.Create(fullPath)
-    if err != nil {
-        return err
-    }
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return err
+	}
 
-    // write compressed data to storage
-    compressed := s.pack(data)
-    _, err = io.Copy(file, bytes.NewReader(compressed))
-    if err != nil {
-        return err
-    }
+	// write compressed data to storage
+	compressed := s.pack(data)
+	_, err = io.Copy(file, bytes.NewReader(compressed))
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
-
 
 func (s *Storage) Delete() {}
 
