25ed011261e7d93320d91666bc3b6af9f1279d42
gfxv <superfreezu@gmail.com>
2024-07-24 22:22:54 +0300

    feat!: reworked file tree building method

--- core/file.go
+++ core/file.go
@@ -1,65 +1,44 @@
 package core
 
 import (
-    "os"
-    "path/filepath"
+	"os"
+	"path/filepath"
 )
 
+// NewTree creates directory hierarchy.
+func NewTree(root string) ([]string, error) {
+	var err error
+	nodes := make([]string, 0)
+	walkFunc := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-// File represents a node in a directory tree.
-type File struct {
-    FullPath string    
-    FileName string
-    IsDir    bool
-    Children []File   
-}
+		dir, err := isDir(path)
+		if err != nil {
+			return err
+		}
 
-// Create directory hierarchy.
-func NewFileTree(root string) (result *File, err error) {
-    nodes := make(map[string]*File)
-    absRoot, err := filepath.Abs(root)
-    if err != nil {
-	    return
-    }
+		if dir {
+			return nil
+		}
 
-    walkFunc := func(path string, info os.FileInfo, err error) error {
-    	if err != nil {
-    		return err
-    	}   
+		nodes = append(nodes, path)
+		return nil
+	}
+	if err = filepath.Walk(root, walkFunc); err != nil {
+		return nil, err
+	}
 
-	    nodes[path] = &File {
-	    	FullPath: path,
-            FileName: info.Name(),
-            IsDir:    info.IsDir(),
-	    	Children: make([]File, 0),
-	    }
-
-	    return nil
-    }
-
-    if err = filepath.Walk(absRoot, walkFunc); err != nil {
-    	return
-    }
-
-    for path, node := range nodes {
-    	parentPath := filepath.Dir(path)
-    	parent, exists := nodes[parentPath]
-    	if !exists { // If a parent does not exist, this is the root.
-    		result = node
-    	} else {
-    		parent.Children = append(parent.Children, *node)
-    	}
-    }
-    return
+	return nodes, err
 }
 
 // IsDir reports whether path describes a directory.
 // Returns err if path does not exist
 func isDir(path string) (bool, error) {
-    f, err := os.Stat(path) 
-    if err != nil {
-        return false, err
-    }
-
-    return f.IsDir(), nil
+	f, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return f.IsDir(), nil
 }
