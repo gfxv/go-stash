69eea17cd32f8e0c1054555c645dd29d8af8a951
gfxv <superfreezu@gmail.com>
2024-10-08 15:24:54 +0300

    test: add checkError func

--- internal/utils/t_utils.go
+++ internal/utils/t_utils.go
@@ -3,8 +3,10 @@ package utils
 // Utils for tests
 
 import (
+	"github.com/stretchr/testify/assert"
 	"log"
 	"os"
+	"testing"
 )
 
 func CreateParent(path string) {
@@ -17,9 +19,21 @@ func CleanUp(path string) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return
 	}
-
 	err := os.RemoveAll(path)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func CheckError(t *testing.T, err error, expected error) {
+	if err != nil && expected != nil && err.Error() != expected.Error() {
+		assert.ErrorIs(t, err, expected)
+	}
+	if err == nil && expected != nil {
+		assert.NotNil(t, err)
+	}
+	if err != nil && expected == nil {
+		t.Errorf("Expected no error, got: %v", err)
+		assert.NoError(t, err)
 	}
 }
