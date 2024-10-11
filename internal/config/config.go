cd23b2448f12fdf43b8110e41b74dab961c369ee
gfxv <superfreezu@gmail.com>
2024-10-11 15:54:07 +0300

    docs: add documentation for config parsing structs

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,30 +14,92 @@ var (
 	announceNew = flag.Bool("announce-new", false, "allow announce new node request")
 )
 
+// Config holds the configuration settings for whole application.
+// Including settings for env, grpc service and storage service.
+// This struct is used to parse settings directly from yml file.
 type Config struct {
-	Env     string        `yaml:"env" env:"STASH_ENV" env-default:"dev"`
-	GRPC    GRPCConfig    `yaml:"grpc"`
+	// Env defines environment in which app will run.
+	// Acceptable values: prod, dev.
+	Env string `yaml:"env" env:"STASH_ENV" env-default:"dev"`
+
+	// GRPC configuration of grpc service.
+	// See GRPCConfig for more details.
+	GRPC GRPCConfig `yaml:"grpc"`
+
+	// Storage configuration of storage service.
+	// See StorageConfig for more details.
 	Storage StorageConfig `yaml:"cas"`
 }
 
 // TODO: add description for config fields
 // see github.com/ilyakaznacheev/cleaner?tab=readme-ov-file#description
 
+// GRPCConfig holds the configuration settings for the gRPC server.
+//
+// The configuration can be populated from YAML file or environment variables
 type GRPCConfig struct {
-	Port                int           `yaml:"port" env:"STASH_PORT" env-default:"5555"`
-	Timeout             time.Duration `yaml:"timeout" env:"STASH_TIMEOUT" env-default:"10s"`
-	HealthCheckInterval time.Duration `yaml:"health-check-interval" env:"STASH_HEALTH_CHECK_INTERVAL" env-default:"10s""`
-	SyncNode            string        `yaml:"sync-node" env:"STASH_SYNC_NODE"`
-	Nodes               []string      `yaml:"nodes" env:"STASH_NODES" env-separator:";"`
+	// Port specifies the port on which the gRPC server will listen for incoming connections.
+	// Default value is 5555.
+	// Can be set through the `STASH_PORT` environment variable.
+	Port int `yaml:"port" env:"STASH_PORT" env-default:"5555"`
 
+	// Timeout defines the duration before a request is considered to have timed out.
+	// The default value is 10 seconds
+	// Can be set via the `STASH_TIMEOUT` environment variable.
+	Timeout time.Duration `yaml:"timeout" env:"STASH_TIMEOUT" env-default:"10s"`
+
+	// HealthCheckInterval sets the interval for health check pings
+	// to be sent to the nodes in the system. This helps ensure that nodes
+	// are responsive and can handle requests.
+	// The default interval is 10 seconds
+	// Can be set via the `STASH_HEALTH_CHECK_INTERVAL` environment variable.
+	HealthCheckInterval time.Duration `yaml:"health-check-interval" env:"STASH_HEALTH_CHECK_INTERVAL" env-default:"10s""`
+
+	// SyncNode identifies the specific node that should be synchronized with.
+	// This field can be set through the `STASH_SYNC_NODE` environment variable
+	// and may be left empty if synchronization is not needed.
+	SyncNode string `yaml:"sync-node" env:"STASH_SYNC_NODE"`
+
+	// Nodes is a list of nodes that the gRPC server can communicate with.
+	// This field can be populated with multiple values passed to env. separated by a semicolon (`;`)
+	Nodes []string `yaml:"nodes" env:"STASH_NODES" env-separator:";"`
+
+	// AnnounceNewNode is a boolean flag that indicates whether the server should
+	// announce the addition of a new node to the system.
+	// If set to true, the server will broadcast the new node's presence to other nodes in the network.
 	AnnounceNewNode bool
 }
 
+// StorageConfig holds the configuration settings for the storage system.
+//
+// This struct provides options for configuring the storage path, compression,
+// and replication settings. Configuration values can be set through YAML file or environment variables
 type StorageConfig struct {
-	Path                       string `yaml:"path" env:"STASH_PATH" env-default:"./stash/"`
-	CompressionLevel           int    `yaml:"compression-level" env:"STASH_COMPRESSION_LEVEL" env-default:"0"` // TODO: <-- ???
-	ReplicationFactor          int    `yaml:"replication-factor" env:"STASH_REPLICATION_FACTOR" env-default:"0"`
-	AllowServerSideCompression bool   `yaml:"allow-server-side-compression" env:"STASH_ALLOW_SERVER_SIDE_COMPRESSION" env-default:"false"` // TODO: <-- ???
+	// Path specifies the directory path where storage data will be located.
+	// The default path is `./stash/`. This can be configured using the `STASH_PATH` environment variable.
+	Path string `yaml:"path" env:"STASH_PATH" env-default:"./stash/"`
+
+	// CompressionLevel Defines the level of compression to be applied to the stored data.
+	//
+	// NOTE: Different levels of compression are not implemented yet
+	//
+	// The default is `0`, and this can be set via the `STASH_COMPRESSION_LEVEL` environment variable.
+	CompressionLevel int `yaml:"compression-level" env:"STASH_COMPRESSION_LEVEL" env-default:"0"`
+
+	// ReplicationFactor indicates the number of replicas for each piece of stored data.
+	// A replication factor of `0` implies no replication, while higher values
+	// provide redundancy for improved reliability and availability.
+	// The default value is `0`
+	// Can be set using the `STASH_REPLICATION_FACTOR` environment variable.
+	ReplicationFactor int `yaml:"replication-factor" env:"STASH_REPLICATION_FACTOR" env-default:"0"`
+
+	// AllowServerSideCompression is a boolean flag that determines whether
+	// server-side compression is permitted. If set to true, the server will
+	// apply compression when storing data, potentially saving disk space at
+	// the cost of increased CPU usage during read/write operations.
+	// The default is `false`
+	// Can be configured using the `STASH_ALLOW_SERVER_SIDE_COMPRESSION` environment variable.
+	AllowServerSideCompression bool `yaml:"allow-server-side-compression" env:"STASH_ALLOW_SERVER_SIDE_COMPRESSION" env-default:"false"` // TODO: <-- ???
 }
 
 func MustLoad() *Config {
