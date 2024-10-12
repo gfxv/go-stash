2a8ea15cfd03a8eefe427e7030e3dc4f9a077c24
gfxv <superfreezu@gmail.com>
2024-10-12 14:35:55 +0300

    docs: add small helper comments

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,12 +19,14 @@ const (
 )
 
 func main() {
-
+	// Load application configuration
 	cfg := config.MustLoad()
-	logger := setupLogger(cfg.Env)
 
+	// Set up logging
+	logger := setupLogger(cfg.Env)
 	cfg.Validate(logger)
 
+	// Prepare options
 	storageOpts := cas.StorageOpts{
 		BaseDir:           cfg.Storage.Path,
 		PathFunc:          cas.DefaultTransformPathFunc,
@@ -32,7 +34,6 @@ func main() {
 		Unpack:            cas.ZLibUnpack,
 		ReplicationFactor: cfg.Storage.ReplicationFactor,
 	}
-
 	appOpts := &app.ApplicationOpts{
 		GRPCOpts:    cfg.GRPC,
 		StorageOpts: storageOpts,
@@ -40,24 +41,31 @@ func main() {
 
 	application := app.NewApp(logger, appOpts)
 
+	// Channel to notify when the GRPC server is ready
 	notifyReady := make(chan bool, 2)
 	go func() {
 		application.GRPC.MustRun(notifyReady)
 	}()
 
+	// Wait for the GRPC server to be ready
 	<-notifyReady
 
 	go func() {
+		// Run the sender service
 		application.Sender.MustRun(notifyReady)
 	}()
 
+	// Wait for the sender service to be ready
 	<-notifyReady
 
+	// Channel to listen for OS termination signals
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
 
+	// Wait for a termination signal
 	<-stop
 
+	// Stop the GRPC server gracefully
 	application.GRPC.Stop()
 	log.Println("Gracefully stopped")
 }
